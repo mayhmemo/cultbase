0177256d018ad8c4a940678a265d238af5a5ed8a
Mateus Vinícius Miotto <mateusbpk@gmail.com>
2023-09-10 15:26:36 -0300

    WIP: Notifications Page
    Iniciado página de notificações

--- lib/pages/notifications_page.dart
+++ lib/pages/notifications_page.dart
@@ -1,5 +1,6 @@
-import 'package:cultbase/pages/components/side_bar.dart';
 import 'package:flutter/material.dart';
+
+import 'components/side_bar.dart';
 
 class NotificationsPage extends StatefulWidget {
   const NotificationsPage({super.key});
@@ -15,7 +16,49 @@ class _NotificationsPageState extends State<NotificationsPage> {
       appBar: AppBar(
         title: const Text('Notifications'),
       ),
+      body: Center(
+        child: ListView(children: const <Widget>[
+          Notifications(),
+          Notifications(),
+        ]),
+      ),
       drawer: const SideBar(),
+    );
+  }
+}
+
+class Notifications extends StatelessWidget {
+  const Notifications({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          const ListTile(
+            leading: Icon(Icons.album),
+            title: Text('The Enchanted Nightingale'),
+            subtitle: Text('Music by Julie Gable. Lyrics by Sidney Stein.'),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: <Widget>[
+              TextButton(
+                child: const Text('BUY TICKETS'),
+                onPressed: () {/* ... */},
+              ),
+              const SizedBox(width: 8),
+              TextButton(
+                child: const Text('LISTEN'),
+                onPressed: () {/* ... */},
+              ),
+              const SizedBox(width: 8),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
